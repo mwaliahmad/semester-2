d4b793025cfd6b454696c39b021706e66367e2da
Muhammad Wali Ahmad <waliaslam2002@gmail.com>
2023-07-09 22:59:00 +0500

    Start Menu

---
+++ FireBuddy/FireBuddy/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace FireBuddy
+{
+    partial class start
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(start));
+            this.btns = new System.Windows.Forms.TableLayoutPanel();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_opt = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btns.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btns
+            // 
+            this.btns.AutoSize = true;
+            this.btns.BackColor = System.Drawing.Color.Transparent;
+            this.btns.ColumnCount = 1;
+            this.btns.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.btns.Controls.Add(this.btn_start, 0, 0);
+            this.btns.Controls.Add(this.btn_opt, 0, 1);
+            this.btns.Controls.Add(this.btn_exit, 0, 2);
+            this.btns.Location = new System.Drawing.Point(259, 201);
+            this.btns.Name = "btns";
+            this.btns.RowCount = 3;
+            this.btns.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.btns.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.btns.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.btns.Size = new System.Drawing.Size(257, 169);
+            this.btns.TabIndex = 0;
+            // 
+            // btn_start
+            // 
+            this.btn_start.BackColor = System.Drawing.Color.Red;
+            this.btn_start.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_start.Font = new System.Drawing.Font("Ink Free", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_start.ForeColor = System.Drawing.Color.DarkBlue;
+            this.btn_start.Location = new System.Drawing.Point(3, 3);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(251, 50);
+            this.btn_start.TabIndex = 1;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = false;
+            // 
+            // btn_opt
+            // 
+            this.btn_opt.BackColor = System.Drawing.Color.Red;
+            this.btn_opt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_opt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_opt.Font = new System.Drawing.Font("Ink Free", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_opt.ForeColor = System.Drawing.Color.DarkBlue;
+            this.btn_opt.Location = new System.Drawing.Point(3, 59);
+            this.btn_opt.Name = "btn_opt";
+            this.btn_opt.Size = new System.Drawing.Size(251, 50);
+            this.btn_opt.TabIndex = 1;
+            this.btn_opt.Text = "Options";
+            this.btn_opt.UseVisualStyleBackColor = false;
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.BackColor = System.Drawing.Color.Red;
+            this.btn_exit.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_exit.Font = new System.Drawing.Font("Ink Free", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_exit.ForeColor = System.Drawing.Color.DarkBlue;
+            this.btn_exit.Location = new System.Drawing.Point(3, 115);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(251, 51);
+            this.btn_exit.TabIndex = 1;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = false;
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.BackColor = System.Drawing.Color.Transparent;
+            this.title.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.title.Font = new System.Drawing.Font("Ink Free", 72F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.Red;
+            this.title.Location = new System.Drawing.Point(0, 0);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(488, 118);
+            this.title.TabIndex = 1;
+            this.title.Text = "Fire Buddy";
+            this.title.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.title);
+            this.panel1.Location = new System.Drawing.Point(158, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(488, 118);
+            this.panel1.TabIndex = 2;
+            // 
+            // start
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FireBuddy.Properties.Resources.welcome;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btns);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "start";
+            this.Text = "Fire Buddy";
+            this.Load += new System.EventHandler(this.start_Load);
+            this.btns.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel btns;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_opt;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ FireBuddy/FireBuddy/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FireBuddy
+{
+    public partial class start : Form
+    {
+        public start()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Load(object sender, EventArgs e)
+        {
+            Stream str = Properties.Resources.alexander_nakarada_chase;
+            SoundPlayer snd = new SoundPlayer(str);
+            snd.Play();
+        }
+    }
+}
